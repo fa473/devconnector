7f85cf78d7649e35633852a95cc590ef5f868447
fa473 <fa473@users.noreply.github.com>
2018-06-30 19:32:37 -0700

    implement logout

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import jwt_decode from 'jwt-decode'
 import setAuthToken from './utils/setAuthToken'
-import { setCurrentUser } from './actions/authActions'
+import { setCurrentUser, logoutUser } from './actions/authActions'
 import store from './store'
 
 import Navbar from './components/layout/Navbar'
@@ -23,6 +23,16 @@ if (localStorage.jwtToken) {
   const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded))
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser)
+    // TODO: clear current profile
+    // redirect to login
+    window.location.href = '/login'
+  }
 }
 
 class App extends Component {
