c0352b010de16d9ea4d84ac2cade569f086482fd
fa473 <fa473@users.noreply.github.com>
2018-07-01 18:50:30 -0700

    create profile component and route

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,31 +1,58 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import TextFieldGroup from '../common/TextFieldGroup'
 
 class CreateProfile extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      displaySocialInput: false,
+      handle: '',
+      company: '',
+      website: '',
+      location: '',
+      status: '',
+      skills: '',
+      githubusername: '',
+      bio: '',
+      twitter: '',
+      facebook: '',
+      linkedin: '',
+      instagram: '',
+      youtube: '',
+      errors: {}
+    }
   }
 
-  componentWillMount() {}
-
-  componentDidMount() {}
-
-  componentWillReceiveProps(nextProps) {}
-
-  shouldComponentUpdate(nextProps, nextState) {}
-
-  componentWillUpdate(nextProps, nextState) {}
-
-  componentDidUpdate(prevProps, prevState) {}
-
-  componentWillUnmount() {}
-
   render() {
-    return <div />
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <small className="d-block pb-3">* = required field</small>
+              <form action="add-experience.html" />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-CreateProfile.propTypes = {}
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  errors: state.errors
+})
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
 
 export default connect(null)(CreateProfile)
