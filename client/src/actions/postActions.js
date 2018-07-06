dd10a7a8c33147e12dc78d044fbba98218de202c
fa473 <fa473@users.noreply.github.com>
2018-07-06 15:32:14 -0700

    create post feed component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { ADD_POST, GET_ERRORS } from './types'
+import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from './types'
 import convertErrorsForForms from '../validation/convert-errors'
 
 // Add post
@@ -15,5 +15,28 @@ export const addPost = (postData) => async (dispatch) => {
       type: GET_ERRORS,
       payload: convertErrorsForForms(err.response.data)
     })
+  }
+}
+// Get post
+export const getPosts = () => async (dispatch) => {
+  try {
+    dispatch(setpostLoading())
+    const res = await axios.get('/api/posts')
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: GET_POSTS,
+      payload: null
+    })
+  }
+}
+
+// Set loading state
+export const setpostLoading = () => {
+  return {
+    type: POST_LOADING
   }
 }
